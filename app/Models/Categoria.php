48c7245835c43284224b6d1da31745605df55c77
danielcc75 <danielcaberos6@gmail.com>
2026-04-10 13:04:22 -0400

    ultima modificacion

---
+++ app/Models/Categoria.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categoria';
+    protected $primaryKey = 'id_categoria';
+
+    protected $fillable = [
+        'nombre',
+        'imagen',
+    ];
+
+    public function habilidades()
+    {
+        return $this->hasMany(Habilidad::class, 'id_categoria');
+    }
+}
