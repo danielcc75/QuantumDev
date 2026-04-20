cb5c4e5927fa622b98fd34898e6b590ddcff67e9
jhonatan-sch <jhonatanschneiderromero@gmail.com>
2026-04-19 22:22:37 -0400

    Visibilidad de contraseña

--- resources/views/home/scripts-home.blade.php
+++ resources/views/home/scripts-home.blade.php
@@ -451,4 +451,21 @@
             }
         });
     }
+
+    function togglePassword(inputId, iconId) {
+        const input = document.getElementById(inputId);
+        const icon = document.getElementById(iconId);
+
+        if (!input || !icon) return;
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    }
 </script>
